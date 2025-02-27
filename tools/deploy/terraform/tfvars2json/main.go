4f0e98aaeab8d8576262c3427ca1ba78d388ebd7
rileykk <rileykk@jpl.nasa.gov>
2025-02-26 16:23:33 -0800

    Copyright headers

--- tools/deploy/terraform/tfvars2json/main.go
+++ tools/deploy/terraform/tfvars2json/main.go
@@ -1,4 +1,4 @@
-// Copyright 2024 California Institute of Technology (Caltech)
+// Copyright 2025 California Institute of Technology (Caltech)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
